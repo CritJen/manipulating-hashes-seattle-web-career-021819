bfe0c4f9de1787c2f1594048d0538644addc6e13
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-04 13:54:12 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,6 @@ def first_challenge
 contacts.each do |person, data|
   data.delete_if{|attribute, value| value =="strawberry"}
 end
-
+end
   #remember to return your newly altered contacts hash!
   contacts
-end
